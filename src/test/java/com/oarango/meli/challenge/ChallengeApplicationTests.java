d67dac828a7cf4180d6d4c123888f13c8becf69c
Óscar Alberto Arango Vásquez <oarangov@gmail.com>
2024-07-09 19:43:03 -0500

    Commit inicial. Creación de proyecto

---
+++ src/test/java/com/oarango/meli/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oarango.meli.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
